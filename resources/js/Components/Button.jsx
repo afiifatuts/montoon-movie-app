ed4a191aee885877e8bca698d87a6b463506f381
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2024-02-29 02:01:21 +0700

    do:slicing component button

--- resources/js/Components/Button.jsx
+++ resources/js/Components/Button.jsx
@@ -1,6 +1,12 @@
+import PropTypes from 'prop-types';
+Button.propTypes={
+
+}
+
 export default function Button({
     type='submit',
     className = '',
+    variant ='primary',
     disabled,
     children,
     ...props }) {
@@ -8,8 +14,8 @@ export default function Button({
         <button
             {...props}
             className={
-                `inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150 ${
-                    disabled && 'opacity-25'
+                `rounded-2xl py-[13px] text-center w-full btn-${variant} ${
+                    disabled && 'opacity-30'
                 } ` + className
             }
             disabled={disabled}
