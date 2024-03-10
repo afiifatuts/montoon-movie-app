805bd346d5be5337cc4c55f3f6e2282ed2db2afc
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2024-03-11 03:07:53 +0700

    feat: integration detail movie

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\User\DashboardController;
+use App\Http\Controllers\User\MovieController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -47,6 +48,7 @@ Route::redirect('/', '/login');
 
 Route::middleware(['auth', 'role:user'])->prefix('dashboard')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('movie/{movie:slug}', [MovieController::class, 'show'])->name('user.dashboard.movie.show');
 });
 
 
